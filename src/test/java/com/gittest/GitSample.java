8eb8eebb14ad0098213403342e56b3308c713a3a
rangu32 <121684622+rangu32@users.noreply.github.com>
2023-01-04 21:52:37 +0530

    Update GitSample.java

--- src/test/java/com/gittest/GitSample.java
+++ src/test/java/com/gittest/GitSample.java
@@ -7,7 +7,11 @@ public class GitSample {
 		for (int j = 0; j < 10; j++) {
 			System.out.println("Helloworld");
 		}
-
+		
+		System.out.println("=====================");
+		for (int j = 0; j < 10; j++) {
+			System.out.println("Selenium automation");
+		}
 	}
 
 }
