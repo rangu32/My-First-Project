339058e971e0a1971d7107994c9e632440ecd16a
rangu32 <rangu32@gmail.com>
2023-01-04 21:20:25 +0530

    added test package with class

---
+++ src/test/java/com/gittest/GitSample.java
@@ -0,0 +1,13 @@
+package com.gittest;
+
+public class GitSample {
+
+	public static void main(String[] args) {
+
+		for (int j = 0; j < 10; j++) {
+			System.out.println("Helloworld");
+		}
+
+	}
+
+}
